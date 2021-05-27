a2672cdb1297911ecda9624a349f981d33fff9a8
Souris Ash <suorees113@gmail.com>
2021-05-27 08:30:20 +0530

    Implemented theme functions

--- theme.c
+++ theme.c
@@ -1,1 +1,60 @@
-#include ""
+#include "theme.h"
+#include "monitor.h"
+
+void monitor_put_wtheme(char c, theme_t theme, int selected = 0) {
+    if (selected) {
+        monitor_put(c, theme.selected_text_color);
+    }
+    else {
+        monitor_put(c, theme.normal_text_color);
+    }
+}
+
+void monitor_clear_wtheme(theme_t theme) {
+    monitor_clear_bg(theme.normal_text_color);
+}
+
+// Write a null-terminated ASCII
+// string to the screen
+void monitor_write_wtheme(char *c, theme_t theme, int selected = 0) {
+    if (selected) {
+        monitor_write(c, theme.selected_text_color);
+    }
+    else {
+        monitor_write(c, theme.normal_text_color);
+    }
+}
+
+void monitor_write_dec_wtheme(u32int n, theme_t theme, int selected = 0) {
+    if (selected) {
+        monitor_write_dec(n, theme.selected_text_color);
+    }
+    else {
+        monitor_write_dec(n, theme.normal_text_color);
+    }
+}
+
+/* For writing with the current theme: */
+
+// Write a single char to screen
+void monitor_put_theme(char c, int selected = 0) {
+    monitor_put_wtheme(c, current_theme, selected);
+}
+
+// Set the current theme
+void monitor_set_theme(theme_t theme) {
+    current_theme = theme;
+}
+
+void monitor_set_theme_name(enum themes theme) {
+    current_theme = avail_themes[(int) theme];
+    current_theme_idx = theme;
+}
+
+void monitor_write_theme(char *c, int selected = 0) {
+    monitor_write_wtheme(c, current_theme, selected);
+}
+
+void monitor_write_dec_theme(u32int n, int selected = 0) {
+    monitor_write_dec_wtheme(n, current_theme, selected);
+}
--- theme.h
+++ theme.h
@@ -3,31 +3,31 @@
 
 #include "themes.h"
 
-theme_t current_theme;
-unsigned int current_theme_idx;
+theme_t current_theme = avail_themes[0];
+enum themes current_theme_idx = 0;
 
 // Write a single char to screen
-void monitor_put_wtheme(char c, theme_t theme);
+void monitor_put_wtheme(char c, theme_t theme, int selected = 0);
 
 void monitor_clear_wtheme(theme_t theme);
 
 // Write a null-terminated ASCII
 // string to the screen
-void monitor_write_wtheme(char *c, theme_t theme);
-void monitor_write_dec_wtheme(u32int n, theme_t theme);
+void monitor_write_wtheme(char *c, theme_t theme, int selected = 0);
+void monitor_write_dec_wtheme(u32int n, theme_t theme, int selected = 0);
 
 /* For writing with the current theme: */
 
 // Write a single char to screen
-void monitor_put_theme(char c);
+void monitor_put_theme(char c, int selected = 0);
 
 // Set the current theme
 void monitor_set_theme(theme_t theme);
-void monitor_set_theme_idx(unsigned int theme_idx);
+void monitor_set_theme_name(enum themes theme);
 
 // Write a null-terminated ASCII
 // string to the screen
-void monitor_write_theme(char *c);
-void monitor_write_dec_theme(u32int n);
+void monitor_write_theme(char *c, int selected = 0);
+void monitor_write_dec_theme(u32int n, int selected = 0);
 
 #endif
